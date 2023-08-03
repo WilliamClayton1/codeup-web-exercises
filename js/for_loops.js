6c552d898706483fabd18645a2e7c9de6323fbb1
William Clayton <william.d.clayton1@gmail.com>
2023-08-03 16:57:54 -0500

    Finished part 3 of the for_loops exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,11 +1,20 @@
+//Loops any number in multiplication from 1-10
 function showMultiplicationTable (x) {
-    let i = 1;
-
-    do {
+    for (let i = 1; i <= 10; i++) {
         let answer = (x * i);
         console.log(`${x} x ${i} = ${answer}`);
-        i++;
-    } while (i <= 10);
+    }
 }
 
-showMultiplicationTable(10)
+showMultiplicationTable(9)
+
+//Loops random number 10 times and determines if number is even or odd
+for (let i = 1; i <= 10; i++) {
+    let random = Math.floor((Math.random() * 200) + 20);
+
+    if(random % 2 === 0) {
+        console.log(`${random} is even`);
+    } else {
+        console.log(`${random} is odd`);
+    }
+}
