48e06219f8987d82743b22ea754e905328c0bc83
William Clayton <william.d.clayton1@gmail.com>
2023-08-03 23:50:12 -0500

    Finished Exercise 4 in for_loops

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,20 +1,64 @@
 //Loops any number in multiplication from 1-10
-function showMultiplicationTable (x) {
-    for (let i = 1; i <= 10; i++) {
-        let answer = (x * i);
-        console.log(`${x} x ${i} = ${answer}`);
-    }
-}
-
-showMultiplicationTable(9)
+// function showMultiplicationTable (x) {
+//     for (let i = 1; i <= 10; i++) {
+//         let answer = (x * i);
+//         console.log(`${x} x ${i} = ${answer}`);
+//     }
+// }
+//
+// showMultiplicationTable(9)
 
 //Loops random number 10 times and determines if number is even or odd
-for (let i = 1; i <= 10; i++) {
-    let random = Math.floor((Math.random() * 200) + 20);
+// for (let i = 1; i <= 10; i++) {
+//     let random = Math.floor((Math.random() * 200) + 20);
+//
+//     if(random % 2 === 0) {
+//         console.log(`${random} is even`);
+//     } else {
+//         console.log(`${random} is odd`);
+//     }
+// }
 
-    if(random % 2 === 0) {
-        console.log(`${random} is even`);
-    } else {
-        console.log(`${random} is odd`);
+for (let i = 1; i < 10; i++) {
+    console.log(i);
+    for (i = 2; i < 10; i++) {
+        let convert = String(i);
+        let repeater = convert.repeat(2);
+        console.log(repeater);
+        for (i = 3; i < 10; i++) {
+            let convert = String(i);
+            let repeater = convert.repeat(3);
+            console.log(repeater);
+            for (i = 4; i < 10; i++) {
+                let convert = String(i);
+                let repeater = convert.repeat(4);
+                console.log(repeater);
+                for (i = 5; i < 10; i++) {
+                    let convert = String(i);
+                    let repeater = convert.repeat(5);
+                    console.log(repeater);
+                    for (i = 6; i < 10; i++) {
+                        let convert = String(i);
+                        let repeater = convert.repeat(6);
+                        console.log(repeater);
+                        for (i = 7; i < 10; i++) {
+                            let convert = String(i);
+                            let repeater = convert.repeat(7);
+                            console.log(repeater);
+                            for (i = 8; i < 10; i++) {
+                                let convert = String(i);
+                                let repeater = convert.repeat(8);
+                                console.log(repeater);
+                                for (i = 9; i < 10; i++) {
+                                    let convert = String(i);
+                                    let repeater = convert.repeat(9);
+                                    console.log(repeater);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
