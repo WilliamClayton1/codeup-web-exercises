48ac190eab2cd6e66afeb6acf3dc301dbb984e74
William Clayton <william.d.clayton1@gmail.com>
2023-08-16 09:45:08 -0500

    finished warm up

--- js/warmup.js
+++ js/warmup.js
@@ -8,3 +8,28 @@ function logUser(stringArgument) {
 
 console.log(logUser('will'));
 
+let userObject = {
+    username: 'JohnDoe123',
+    email: 'JohnDoe123@gmail.com'
+};
+
+console.log(userObject.username);
+console.log(userObject.email);
+
+function logUser(object) {
+    if (typeof object.username !== 'string' || typeof object.email !== 'string') {
+        return false;
+    }
+
+    return `${object.email} has logged in for the day.`;
+}
+
+userObject.userRoles = ['admin', 'moderator', 'buyer', 'seller'];
+
+userObject.userProfile = {
+    userProfileUrl: 'myURLToPictice',
+    userLocation: 'San Antonio, Texas',
+    numLikes: 7
+};
+
+console.log(userObject.userProfile.userProfileUrl);
