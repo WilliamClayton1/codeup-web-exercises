754e5e0b5b6523581146471da1d279907044be82
William Clayton <william.d.clayton1@gmail.com>
2023-08-16 10:38:16 -0500

    finished css selector exercise

--- js/warmup.js
+++ js/warmup.js
@@ -1,10 +1,10 @@
-function logUser(stringArgument) {
-    if (typeof stringArgument !== 'string' || !isNaN(Number(stringArgument))) {
-        return false;
-    }
-
-    return `${stringArgument} has logged in for the day.`;
-}
+// function logUser(stringArgument) {
+//     if (typeof stringArgument !== 'string' || !isNaN(Number(stringArgument))) {
+//         return false;
+//     }
+//
+//     return `${stringArgument} has logged in for the day.`;
+// }
 
 console.log(logUser('will'));
 
