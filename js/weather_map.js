4297116214b15edb3f516c512d8f16c35148c809
William Clayton <william.d.clayton1@gmail.com>
2023-09-13 15:33:57 -0500

    mapbox map populated, working on linking both apis -wc

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,6 +1,6 @@
 "use strict";
 //Current weather for Converse, TX
-const CURRENT_WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?q=Converse,USA&appid=${WEATHER_API_TOKEN}&units=imperial`;
+const CURRENT_WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?q=Dallas,TX,US&appid=${WEATHER_API_TOKEN}&units=imperial`;
 console.log(CURRENT_WEATHER_URL);
 
 $.get(CURRENT_WEATHER_URL).done((data) => {
@@ -38,4 +38,30 @@ $.get(CURRENT_WEATHER_URL).done((data) => {
 
         $("#insert-weather").html(html);
     }
+
+    //location stored in variables
+    let locationName = `${data.city.name}, ${data.city.country}`
+    let location = `${data.city.coord.lon},${data.city.coord.lat}`;
+    let lonLat = location.split(',')
+    $("#currentCity").html(`${locationName}`)
+
+    //Mapbox Map
+    mapboxgl.accessToken = MAPBOX_API_TOKEN;
+    const map = new mapboxgl.Map({
+        container: 'insert-map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v11', // style URL
+        center: lonLat, // starting position [lng, lat]
+        zoom: 2, // starting zoom
+    });
+
+    //marker will populate with a users city and state input
+    geocode(location, MAPBOX_API_TOKEN).then(function(result) {
+        let marker = new mapboxgl.Marker()
+            .setLngLat(lonLat)
+            .addTo(map)
+        map.setCenter(result);
+        map.setZoom(14);
+    });
+
 })
+
